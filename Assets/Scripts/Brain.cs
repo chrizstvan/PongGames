87e9dd738314e5face44fee4ee319ed87581586a
Christian Stevanus <chris.stev30@gmail.com>
2019-02-18 16:05:36 +0700

    Adding Brain and intelegent function

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour 
+{
+    public GameObject paddle;
+    public GameObject ball;
+
+    Rigidbody2D b_RigidBod;
+    float y_velocity;
+    float paddleMinY = 8.6f;
+    float paddleMaxY = 17.6f;
+    float paddlesMaxSpeed = 15;
+
+    public float numSaved = 0;
+    public float numMissed = 0;
+
+    ANN _artificialNeuronNetwork;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _artificialNeuronNetwork = new ANN(6, 1, 1, 4, 0.11);
+        b_RigidBod = ball.GetComponent<Rigidbody2D>();
+	}
+
+    List<double> Run(double ballX, double ballY, double ballVelocX, double ballVelocY, double paddleX, double paddleY, double paddleVelocity, bool train)
+    {
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+        inputs.Add(ballX);
+        inputs.Add(ballY);
+        inputs.Add(ballVelocX);
+        inputs.Add(ballVelocY);
+        inputs.Add(paddleX);
+        inputs.Add(paddleY);
+        outputs.Add(paddleVelocity);
+
+        if (train)
+        {
+            return (_artificialNeuronNetwork.Train(inputs, outputs));
+        }
+        else
+        {
+            return (_artificialNeuronNetwork.CalcOutput(inputs, outputs));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ProcessLearning();
+	}
+
+    void ProcessLearning()
+    {
+        float posY = Mathf.Clamp(paddle.transform.position.y + (y_velocity * Time.deltaTime * paddlesMaxSpeed), paddleMinY, paddleMaxY);
+        paddle.transform.position = new Vector3(paddle.transform.position.x, posY, paddle.transform.position.z);
+
+        List<double> output = new List<double>();
+        int layerMask = 1 << 9;
+        RaycastHit2D hit = Physics2D.Raycast(ball.transform.position, b_RigidBod.velocity, 1000, layerMask);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "tops")
+            {
+                Vector3 reflection = Vector3.Reflect(b_RigidBod.velocity, hit.normal);
+                hit = Physics2D.Raycast(hit.point, reflection, 1000, layerMask);
+            }
+
+            else if (hit.collider.gameObject.tag == "backwall")
+            {
+                float distanceY = (hit.point.y - paddle.transform.position.y);
+
+                output = Run(ball.transform.position.x,
+                             ball.transform.position.y,
+                             b_RigidBod.velocity.x,
+                             b_RigidBod.velocity.y,
+                             paddle.transform.position.x,
+                             paddle.transform.position.y,
+                             distanceY,
+                             true);
+
+                y_velocity = (float)output[0];
+            }
+        }
+        else
+            y_velocity = 0;
+    }
+}
---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour 
+{
+    Vector3 ballStartPosition;
+    Rigidbody2D rb;
+    float speed = 400;
+    public AudioSource blip;
+    public AudioSource blop;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        ballStartPosition = gameObject.transform.position;
+        ResetBall();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.gameObject.tag == "backwall")
+        {
+            blop.Play();
+        }
+        else
+            blip.Play();
+	}
+
+    public void ResetBall()
+    {
+        gameObject.transform.position = ballStartPosition;
+        rb.velocity = Vector3.zero;
+        Vector3 direction = new Vector3(Random.Range(100, 300), Random.Range(100, 100), 0).normalized;
+        rb.AddForce(direction * speed);
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetBall();
+        }
+	}
+}
